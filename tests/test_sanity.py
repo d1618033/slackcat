5e4821ebabbe7fc29d5f0b9d790d53f17d101acd
David S <d1618033@gmail.com>
2021-01-05 22:59:46 +0200

    Fix formatting

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,8 +1,8 @@
-from slackcat.entry_point.main import main
 from click.testing import CliRunner
+
+from slackcat.entry_point.main import main
 
 
 def test_main():
     runner = CliRunner()
     assert runner.invoke(main, ["--help"]).stdout.startswith("Usage: slackcat")
-
